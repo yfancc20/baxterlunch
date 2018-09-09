f5333e6594201347855ead40d3fd6cefdce7b36f
Yifan <jameswu1212@gmail.com>
2018-09-09 22:26:40 +0800

    finish cont

---
+++ src/cont.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+<?php require_once('components/head.php') ?>
+
+<title>Baxter</title>
+
+</head>
+<body>
+
+	<? require_once('components/header.php') ?>
+	<div class="wrapper cont">
+		<div class="content">
+			<div class="paragraph">
+				<div class="title">
+					The Journey to
+				</div>
+				<div class="title">
+					High Performance CRRT
+				</div>
+				<div class="title date">
+					22<span class="super">nd</span> Sep 2018
+				</div>
+				<p class="text">Taipei Marriott Hotel 5F Junior Ballroom 1</p>
+			</div>
+			<div class="dot-line"></div>
+
+			<div class="paragraph">
+				<div class="title">
+					Meet-the-Expert Sessions
+				</div>
+				<div class="title date">
+					22<span class="super">nd</span> - 23<span class="super">rd</span> Sep 2018
+				</div>
+				<p class="text">Coffee Break | Taipei Marriott Hotel 3F - Salon 3/4</p>
+			</div>
+			<div class="dot-line"></div>
+		</div>
+
+		<div class="more text-center">
+			<img src="/img/button-more.png">
+		</div>
+	</div>
+</body>
+</html>
--- src/index.php
+++ src/index.php
@@ -19,7 +19,7 @@
 				<br>
 				<h3 class="title">Meet-the-Expert Sessions</h3>
 
-				<h3 class="date mt-4 px-3 py-2 mx-auto">
+				<h3 class="title date mt-4 px-3 py-2 mx-auto">
 					22<span>nd</span> - 23<span>rd</span> Sep 2018
 				</h3>
 			</div>
