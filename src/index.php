1ce5dac69a985d568e786eff6a1302628a123d95
Yifan <jameswu1212@gmail.com>
2018-09-11 01:33:43 +0800

    fix font-size

--- src/index.php
+++ src/index.php
@@ -14,11 +14,11 @@
 		<div class="welcome">
 			<img src="img/spin.png">
 			<div class="content text-center">
-				<h3 class="title">Baxter Medical Lunch Symposium</h3>
-				<h3 class="title">The Journey to</h3>
-				<h3 class="title">High Performance CRRT</h3>
+				<h3 class="title-md">Baxter Medical Lunch Symposium</h3>
+				<h3 class="title-md">The Journey to</h3>
+				<h3 class="title-md">High Performance CRRT</h3>
 				<br>
-				<h3 class="title">Meet-the-Expert Sessions</h3>
+				<h3 class="title-md">Meet-the-Expert Sessions</h3>
 
 				<h3 class="title date mt-4 px-3 py-2 mx-auto">
 					22<span class="super">nd</span> - 23<span class="super">rd</span> Sep 2018
